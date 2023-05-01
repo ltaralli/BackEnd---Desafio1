51f491fa15afc165cb67f8c61e59dabd941051fa
ltaralli <lisandrotaralli@gmail.com>
2023-05-01 15:16:08 -0300

    Desafio2

--- main.js
+++ main.js
@@ -1,54 +1,116 @@
+import { AsyncResource } from 'async_hooks';
+import fs from 'fs';
+
 class ProductManager{
     constructor(){
-        this.products = []
+        this.path = 'products.json'
+        // this.products = []
         this.index = 0
     }
+    
 
-    getProducts = () => {
-        return this.products
+    async getProducts() {
+       const content = await fs.promises.readFile(this.path)
+       const products = JSON.parse(content)
+       return products; 
     }
 
-    getProductsById = () => {
-
+    async addProduct(title, description, price, thumbnail, code, stock){
+        // SE GENERA EL ID
+        this.index++
+        const id = this.index
+        // OBJETO LITERAL
+        const product = {id, title, description, price, thumbnail, code, stock}
+        // SE OBTIENEN LOS PRODUCTOS
+        const products = await this.getProducts();
+        const siExiste = products.some(product => product.code === code)
+        // VALIDACIONES
+        if (siExiste) return console.log("El código ya fue ingresado, por favor ingresa otro diferente")
+        if (!title || !description || !price || !thumbnail || !code || !stock) return console.log("Por favor, completa todos los datos")
+        // PUSHEO
+        products.push(product)
+        await fs.promises.writeFile(this.path, JSON.stringify(products))
     }
+   
 
-    getProductById = (id) => {
-        const product = this.products.find(product => product.id === id)
-        if (!product) return console.log(`No se encontró ningún producto con el id: ${id}`)
+    async getProductById(id){
+        const products = await this.getProducts();
+        const product = products.find(product => product.id === id)
+        if (!product) {
+            console.log(`No se encontró ningún producto con el id: ${id}`)
+        }
         return product
     }
 
-
-    addProduct = (title, description, price, thumbnail, code, stock) => {
-        this.index++
-        const id = this.index
-        
-        const product = {id, title, description, price, thumbnail, code, stock}
-        
-        const siExiste = this.products.some(product => product.code === code)
-        if (siExiste) return console.log("El código ya fue ingresado, por favor ingresa otro diferente")
-        
-        if (!title || !description || !price || !thumbnail || !code || !stock) return console.log("Por favor, completa todos los datos")
-
-        this.products.push(product)
+    async updateProduct(id, title, description, price, thumbnail, code, stock){
+        const products = await this.getProducts();
+    
+        const index = products.findIndex(product => product.id === id )
+            
+        if(index !== -1){
+            const updatedProduct = {
+                id,
+                title: title || products[index].title, 
+                description: description || products[index].description, 
+                price: price || products[index].price, 
+                thumbnail: thumbnail || products[index].thumbnail, 
+                code: code || products[index].code, 
+                stock: stock || products[index].stock
+            }
+            products[index] = updatedProduct;
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+            console.log(`Se han guardado los cambios del producto con id: ${id}`);
+            return products[index];
+        } else {
+            console.log(`No se encontró ningún producto con el id: ${id}`)
+        }
     }
+    
+    async deleteProduct(id){
+        const products = await this.getProducts();
+        const index = products.findIndex(product => product.id === id )
+        let productDeleted;
+        if(index !== -1){
+            productDeleted = products.splice(index, 1)[0];
+        }
+        await fs.promises.writeFile(this.path, JSON.stringify(products));
+        console.log(`Se eliminó el producto con id: ${id}`);
+    }
+
 }
 
-const manager = new ProductManager()
 
 
 // TESTEO DE FUNCIONAMIENTO
 
-manager.addProduct('producto prueba' , 'Este es un producto de prueba', 200, 'sin imagen', 'abc123', 25 )
+const manager = new ProductManager();
 
-console.log(manager.getProducts())
+await manager.addProduct('producto prueba' , 'Este es un producto de prueba', 200, 'sin imagen', 'abc123', 25 )
 
-console.log('--------------------------')
-
-manager.addProduct('producto prueba' , 'Este es un producto de prueba', 200, 'sin imagen', 'abc123', 25 )
+console.log(await manager.getProducts());
 
 console.log('--------------------------')
 
-manager.getProductById(3)
+console.log('Se agregan mas productos')
+
+await manager.addProduct('producto prueba' , 'Este es un producto de prueba', 200, 'sin imagen', 'ab', 25 )
+await manager.addProduct('producto prueba2' , 'Este es un producto de prueba2', 345, 'sin imagen', 'sndoav', 2 )
+await manager.addProduct('producto prueba3' , 'Este es un producto de prueba3', 2345, 'sin imagen', 'naw82943849', 5 )
+await manager.addProduct('producto prueba4' , 'Este es un producto de prueba4', 345, 'sin imagen', '9283741huffsd', 29 )
 
 console.log('--------------------------')
+
+console.log('Se busca un producto por ID')
+console.log(await manager.getProductById(3))
+
+console.log('--------------------------')
+
+console.log(await manager.updateProduct(1, "producto prueba modificado con funcion", "descripcion"))
+
+console.log('--------------------------')
+
+console.log(await manager.getProducts());
+
+console.log('--------------------------')
+
+console.log(await manager.deleteProduct(2));
